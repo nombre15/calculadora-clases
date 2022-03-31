253c85e8c19eb569d61bebec96bcdac915843346
nombre <81451283+nombre15@users.noreply.github.com>
2022-03-31 15:46:05 -0500

    Update calculadora.cpp

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <math.h>
-#include <time.h>
+#include <conio.h>
+#include <windows.h>
 #include "Calculadora.h"
 #include "Varios.h"
 using namespace std;
@@ -8,86 +9,110 @@ using namespace std;
 int main(){
 
     int opcion, opcion2;
+    char tecla;
 
     calculadora CalculadoraObjeto;
     Varios VariosObjeto;
 
+    inicio:
+
     cout << "1: Calculadora" << endl << "2: Varios" << endl;
     cin >> opcion;
 
-    if(opcion == 1){
+    system("CLS");
 
-        do{
-            cout << "\nEscribe x: ";
-            cin >> x;
-            cout << "Escribe y: ";
-            cin >> y;
-            cout << endl;
+    switch(opcion){
 
-            cout << "Opciones: " << endl << "1 - Suma\n2 - Resta\n3 - Division\n4 - Multiplicacion\n5 - Elevar\n6 - Raiz\n7 - Logaritmo" << endl;
-            cin >> opcion;
+        case 1:
 
-            switch(opcion2){
+            do{
+                cout << "Escribe x: ";
+                cin >> x;
+                cout << "Escribe y: ";
+                cin >> y;
+                cout << endl;
+                system("CLS");
 
-                case 1: CalculadoraObjeto.suma();
-                cout << endl << x << " + " << y << " = " << resultado;
-                break;
+                cout << "Opciones: " << endl << "1 - Suma\n2 - Resta\n3 - Division\n4 - Multiplicacion\n5 - Elevar\n6 - Raiz\n7 - Logaritmo" << endl;
+                cin >> opcion2;
+                system("CLS");
 
-                case 2: CalculadoraObjeto.resta();
-                cout << endl << x << " - " << y << " = " << resultado;
-                break;
+                switch(opcion2){
 
-                case 3: CalculadoraObjeto.division();
-                cout << endl << x << " / " << y << " = " << resultado;
-                break;
+                    case 1: CalculadoraObjeto.suma();
+                        cout << x << " + " << y << " = " << resultado;
+                    break;
 
-                case 4: CalculadoraObjeto.multiplicacion();
-                cout << endl << x << " x " << y << " = " << resultado;
-                break;
+                    case 2: CalculadoraObjeto.resta();
+                        cout << x << " - " << y << " = " << resultado;
+                    break;
 
-                case 5: CalculadoraObjeto.elevar();
-                cout << endl << x << " ^ " << y << " = " << resultado;
-                break;
+                    case 3: CalculadoraObjeto.division();
+                        cout << x << " / " << y << " = " << resultado;
+                    break;
 
-                case 6: CalculadoraObjeto.raiz();
-                cout << endl << "cuadrado de " << x << " = " << resultado;
-                cout << endl << "cuadrado de " << y << " = " << resultado2;
-                break;
+                    case 4: CalculadoraObjeto.multiplicacion();
+                        cout << x << " x " << y << " = " << resultado;
+                    break;
 
-                case 7: CalculadoraObjeto.logaritmo();
-                cout << endl << "log " << x << " = " << resultado;
-                cout << endl << "log " << y << " = " << resultado;
-                break;
+                    case 5: CalculadoraObjeto.elevar();
+                        cout << x << " ^ " << y << " = " << resultado;
+                    break;
 
-            }
+                    case 6: CalculadoraObjeto.raiz();
+                        cout << "cuadrado de " << x << " = " << resultado;
+                        cout << "\ncuadrado de " << y << " = " << resultado2;
+                    break;
 
-            cout << endl;
-        } while(opcion == 1);
-    }
+                    case 7: CalculadoraObjeto.logaritmo();
+                        cout << "log " << x << " = " << resultado;
+                        cout << "\nlog " << y << " = " << resultado;
+                    break;
+                }
 
-    if(opcion == 2){
+                cout << "\n\nPresiona Enter para repetir, o ESC para salir" << endl;
+                tecla = getch();
+                system("CLS");
 
-        do{
-            cout << "\nOpciones: " << endl << "1 - Promedio\n2 - Numero al azar\n3 - Lista\n4 - Imprimir lista" << endl;
-            cin >> opcion2;
-            cout << endl;
+                if(tecla == 27){
+                    goto inicio;
+                }
 
-            switch(opcion2){
+            }while(tecla == 13);
+        break;
 
-                case 1: VariosObjeto.promedio();
-                break;
+        case 2:
 
-                case 2: VariosObjeto.azar();
-                break;
+            do{
+                cout << "Opciones: " << endl << "1 - Promedio\n2 - Numero al azar\n3 - Lista\n4 - Imprimir lista" << endl;
+                cin >> opcion2;
+                cout << endl;
+                system("CLS");
 
-                case 3: VariosObjeto.lista();
-                break;
+                switch(opcion2){
 
-                case 4: VariosObjeto.PrintList();
-                break;
-            }
+                    case 1: VariosObjeto.promedio();
+                    break;
 
-            cout << endl;
-        }while(opcion == 2);
+                    case 2: VariosObjeto.azar();
+                    break;
+
+                    case 3: VariosObjeto.lista();
+                    break;
+
+                    case 4: VariosObjeto.PrintList();
+                    break;
+                }
+
+            cout << "\n\nPresiona Enter para repetir, o ESC para salir" << endl;
+            tecla = getch();
+            system("CLS");
+
+             if(tecla == 27){
+                 goto inicio;
+             }
+
+            }while(tecla == 13);
+        break;
     }
 }
